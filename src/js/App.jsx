ff2c89a0c77772d498ae7ba01f1f9e9b5f4af674
Mydayyy <mydayyy@outlook.com>
2018-05-12 23:11:31 +0200

    Formatting fixes

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { hot } from "react-hot-loader"
+import {hot} from "react-hot-loader"
 
 import Button from "~/components/Button/Button";
 
@@ -8,22 +8,25 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-			counter: 0
-		};
+            counter: 0
+        };
     }
 
-	onButtonClick() {
-		this.setState({
-			counter: this.state.counter + 1
-		});
-	}
+    onButtonClick() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    }
 
     render() {
         return (
-			<div id="app">
-				<Button name={"Clicked: " + this.state.counter} onClick={this.onButtonClick.bind(this)} secondary={true}></Button>
-			</div>
-		);
+            <div id="app">
+                <Button name={"Clicked: " + this.state.counter}
+                        onClick={this.onButtonClick.bind(this)}
+                        secondary={true}/>
+            </div>
+        );
     }
 }
+
 export default hot(module)(App);
