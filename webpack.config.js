fadc83b706eb12612b7bd4969aa88f8077805c93
Mydayyy <mydayyy@outlook.com>
2018-05-16 19:30:09 +0200

    Implemented an optional static site generator

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 const path = require("path");
 
+const PUBLIC_PATH = "/";
+const ENABLE_STATIC_SITE_GENERATOR = true;
+
+const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
+const StaticSiteGeneratorPlugin = require("static-site-generator-webpack-plugin");
+
 module.exports = (env, argv) => {
     const isDev = argv.mode === "development";
 
     const htmlWebPackPlugin = new HtmlWebPackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html"
+        template: "./src/js/index.ejs",
+        filename: "./index.html",
+        inject: false
     });
 
     const miniCssExtractPlugin = new MiniCssExtractPlugin({
@@ -18,48 +25,81 @@ module.exports = (env, argv) => {
         chunkFilename: isDev ? "[id].css" : "[id].[hash].css"
     });
 
+    const staticSiteGeneratorPlugin = new StaticSiteGeneratorPlugin({
+        globals: {
+            window: {}
+        },
+        crawl: true
+    });
+
+    const providePlugin = new webpack.ProvidePlugin({
+        _: "underscore"
+    });
+
+    const definePlugin = new webpack.DefinePlugin({
+        "process.env": {
+            PUBLIC_PATH: PUBLIC_PATH
+        }
+    });
+
+    let plugins = [ENABLE_STATIC_SITE_GENERATOR && !isDev ? staticSiteGeneratorPlugin : htmlWebPackPlugin, miniCssExtractPlugin, providePlugin, definePlugin];
+
     return {
         entry: "./src/js/index.js",
+        output: {
+            libraryTarget: "umd",
+            publicPath: PUBLIC_PATH
+        },
         resolve: {
-            extensions: [
-                ".js", ".jsx"
-            ],
+            extensions: [".js", ".jsx"],
             alias: {
                 ["~"]: path.resolve(__dirname, "src/js")
             }
         },
         module: {
-            rules: [{
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.s?[ac]ss$/,
+                    use: [
+                        {
+                            loader: isDev ? "style-loader" : MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                importLoaders: 1
+                            }
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                sourceMap: false,
+                                ident: "postcss",
+                                plugins: () => [require("autoprefixer")({})]
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                includePaths: [path.resolve(__dirname, "src/scss")]
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.ejs$/,
+                    loader: "ejs-loader"
                 }
-            }, {
-                test: /\.s?[ac]ss$/,
-                use: [{
-                    loader: isDev ? "style-loader" : MiniCssExtractPlugin.loader
-                }, {
-                    loader: "css-loader",
-                    options: {
-                        importLoaders: 1
-                    }
-                }, {
-                    loader: "postcss-loader",
-                    options: {
-                        sourceMap: false,
-                        ident: "postcss",
-                        plugins: () => [require("autoprefixer")({})]
-                    }
-                }, {
-                    loader: "sass-loader",
-                    options: {
-                        includePaths: [path.resolve(__dirname, "src/scss")]
-                    }
-                }],
-            }]
+            ]
         },
-        plugins: [htmlWebPackPlugin, miniCssExtractPlugin],
+        plugins: plugins,
         optimization: {
             minimizer: [
                 new UglifyJsPlugin({
@@ -74,10 +114,11 @@ module.exports = (env, argv) => {
                     styles: {
                         name: "styles",
                         test: /\.css$/,
-                        chunks: "all"
+                        chunks: "all",
+                        enforce: false
                     }
                 }
             }
         }
     };
-}
+};
